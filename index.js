2b95e5bf3b30207727daa6ead8a472c65d1e7780
md sahadat hossin <mdsahadathossin778@gmail.com>
2021-11-19 22:36:47 +0600

    all done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-// middleware
+// middleware useing heer
 app.use(cors());
 app.use(express.json());
 
@@ -15,7 +15,7 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+// async funtion
 async function run() {
   try {
     await client.connect();
